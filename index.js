f7d602101bba2e5c34c88d36016c1bb415d4cf58
batovpasha <batovpasha@gmail.com>
2019-03-15 00:34:15 +0200

    Remove old index.js file and rename proxyServer.js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('Everything starts correctly!');
+const http = require('http');
+
+const PORT = 8000;
+
+const httpProxyServer = http.createServer((clientReq, clientRes) => {
+  const options = {
+    hostname: clientReq.headers.host,
+    path: clientReq.url,
+    method: clientReq.method
+  };
+
+  const proxyRequest = http.request(options, (serverRes) => {
+    clientRes.headers = serverRes.headers;
+    serverRes.pipe(clientRes, { end: true });
+  });
+
+  clientReq.pipe(proxyRequest, { end: true });
+});
+
+httpProxyServer.listen(PORT);
