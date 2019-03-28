b9f1e6013b2713201602be6907df298c26892664
batovpasha <batovpasha@gmail.com>
2019-03-29 01:53:21 +0200

    Remove tralling whitespaces

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const https = require('https');
 const PORT = 8000;
 
 const httpProxyServer = http.createServer((clientReq, clientRes) => {
-  const protocol = clientReq.url.startsWith('https') ? https : http; 
-  
+  const protocol = clientReq.url.startsWith('https') ? https : http;
+
   const options = {
     hostname: clientReq.headers.host,
     path: clientReq.url,
@@ -17,7 +17,7 @@ const httpProxyServer = http.createServer((clientReq, clientRes) => {
   const proxyRequest = protocol.request(options, (serverRes) => {
     serverRes.pipe(clientRes, { end: true });
   });
-  
+
   clientReq.pipe(proxyRequest, { end: true });
 });
 
