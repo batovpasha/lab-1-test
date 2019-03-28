17f5d304b470c08029068cc5c8f808803de554b2
batovpasha <batovpasha@gmail.com>
2019-03-29 01:42:27 +0200

    Refactor server realization

--- index.js
+++ index.js
@@ -6,26 +6,18 @@ const https = require('https');
 const PORT = 8000;
 
 const httpProxyServer = http.createServer((clientReq, clientRes) => {
-  const protocol = clientReq.url.split(':')[0];
-
+  const protocol = clientReq.url.startsWith('https') ? https : http; 
+  
   const options = {
     hostname: clientReq.headers.host,
     path: clientReq.url,
     method: clientReq.method
   };
 
-  let proxyRequest;
-
-  if (protocol === 'https') {
-    proxyRequest = https.request(options, (serverRes) => {
-      serverRes.pipe(clientRes, { end: true });
-    });
-  } else {
-    proxyRequest = http.request(options, (serverRes) => {
-      serverRes.pipe(clientRes, { end: true });
-    });
-  }
-
+  const proxyRequest = protocol.request(options, (serverRes) => {
+    serverRes.pipe(clientRes, { end: true });
+  });
+  
   clientReq.pipe(proxyRequest, { end: true });
 });
 
