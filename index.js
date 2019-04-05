3efc27cefd63fd4d934ea657003e3fe93cd6ca0b
batovpasha <batovpasha@gmail.com>
2019-04-05 09:48:36 +0300

    Fix http proxy realization

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ const PORT = 8000;
 const httpProxyServer = http.createServer((clientReq, clientRes) => {
   const protocol = clientReq.url.startsWith('https') ? https : http;
 
+  console.log(clientReq.url);
+
   const options = {
-    hostname: clientReq.headers.host,
-    path: clientReq.url,
+    headers: clientReq.headers,
     method: clientReq.method
   };
 
-  const proxyRequest = protocol.request(options, (serverRes) => {
+  const proxyRequest = protocol.request(clientReq.url, options, (serverRes) => {
+    clientRes.writeHead(
+      serverRes.statusCode,
+      serverRes.statusMessage,
+      serverRes.headers
+    );
     serverRes.pipe(clientRes, { end: true });
   });
 
