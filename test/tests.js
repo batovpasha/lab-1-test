1fe1d69d3b5cdb647960cae1a5c3929c3e63289f
batovpasha <batovpasha@gmail.com>
2019-06-17 19:32:18 +0300

    Rewrote transposing function

--- test/tests.js
+++ test/tests.js
@@ -1,24 +1,3 @@
 'use strict';
 
-//console.log('Passed all tests!');
-
-const transpose = (matrix) => {
-  const transposedMatrix = []
-  matrix[0].forEach((el, i) => {
-    const newRow = matrix.reduce((acc, val, j) => 
-      (acc.push(matrix[j][i]), acc)
-    , []);
-    transposedMatrix.push(newRow);
-  });
-  return transposedMatrix;
-};
-
-const res = transpose(
-  [
-    [1, 2],
-    [3, 4],
-    [5, 6]
-  ]
-);
-
-console.log(res);
+console.log('Passed all tests!');
